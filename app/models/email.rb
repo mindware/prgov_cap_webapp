27815ab0c837f38bd10508dd3a4c73156fe4f2be
Andrés Colón <andres@thoughtware.tv>
2014-11-24 15:56:40 -0400

    Adding base db models, GMQ API, and Email representation.

---
+++ app/models/email.rb
@@ -0,0 +1,206 @@
+# A class that represents emails in the storage.
+# Email addresses are validated by sending an email to the user.
+# Once an email has been validated as a delivery mechanism, we can
+# confirm it by using this class.
+# by: Andrés Cólon Pérez 2014 / for PR.gov DevTeam
+# Require the GMQ CAP API class
+require PADRINO_ROOT + "/app/models/base"
+require PADRINO_ROOT + '/app/helpers/cap_helper'
+require 'securerandom'
+require 'base64'
+require 'json'
+
+class Email < PRgov::Base
+
+    include PRgovCAPWebApp::App::CAPHelper
+
+    attr_reader :MAX_MINUTES, :MAX_MINUTES_IN_SECONDS
+
+    attr_accessor :address,             # The user's email address
+                  :confirmation_code,   # The generated confirmation code
+                                        # to validate an email address.
+                  :confirmed,           # Flag that determines if email
+                                        # has been confirmed.
+                  :created_at,          # Date of request
+                  :updated_at,          # Last update/lookup.
+                  :sent_at,             # Time the email was requested
+                                        # by the webapp to be sent.
+                  :IP,                  # IP of the requestor
+                  :requests             # Counter that determines how many
+                                        # requests this email has performed.
+
+    # What it is:
+    # This value is used both as a limiter and also as a
+    # presentation to the user. Modifying this value, modifies
+    # the number of minutes that a user is presented with in the UI.
+    # What it does:
+    # Wait at most MAX_MINUTES minutes before allowing users to request
+    # a resend of an email to confirm their address. This
+    # is to give time to the mail server to empty its queue.
+    MAX_MINUTES = 10
+    # Turns MAX_MINUTES minutes into seconds
+    MAX_MINUTES_IN_SECONDS = 60 * MAX_MINUTES
+
+    # Newly created objects from user input
+    def self.create(params)
+      # The following parameters are allowed to be input by the user
+      # everything else will be discarded from the user params
+      # by the validate_email_creation_parameters() method
+      whitelist = ["address", "IP"]
+      params = validate_email_creation_parameters(params, whitelist)
+
+      email = self.new.setup(params)
+      # Add important system defined parameters here:
+      email.confirmation_code   = generate_code()
+      email.confirmed           = false
+      email.created_at          = Time.now.utc
+      email.updated_at          = Time.now.utc
+      email.sent_at             = nil
+      # email.request
+      return email
+    end
+
+    # Email addresses cannot be duplicated in the database.
+    # If an attempt to create a duplicate is performed, an
+    # exception must be raised.
+
+    # Returns true if confirmed, false otherwise.
+    def confirmed?
+      self.confirmed == true
+    end
+
+    def registered?
+      !self.confirmed?
+    end
+
+    # returns the count down number in minutes
+    def resend_countdown
+      time = ((Time.parse(self.sent_at.to_s) + MAX_MINUTES_IN_SECONDS) - Time.now.utc) / 60
+      time.to_i
+    end
+
+    # returns minutes taking into consideration
+    # internationalization ("1 minute" or "2 minutos")
+    def countdown_in_minutes
+      time = resend_countdown
+      if(time <= 1 and time >= 0)
+        time = 1
+        minutes = "#{i18n_t("email_sent.minute")}"
+      else
+        # make minute/minuto plural by adding an s
+        minutes = "#{i18n_t("email_sent.minute")}s"
+      end
+      "#{time} #{minutes}"
+    end
+
+    def should_resend?
+      # if email is confirmed we shouldn't resend
+      return false if confirmed?
+      # if email doesn't have a sent_at date we must
+      # send an email.
+      return true if self.sent_at.nil?
+      # if email confirmation is older than MAX_MINUTES_IN_SECONDS
+      time = (Time.parse(self.sent_at) - (Time.now.utc - (MAX_MINUTES_IN_SECONDS)))
+      if (time <= 0)
+        return true
+      else
+        return false
+      end
+    end
+
+    # Tries to find a record, if nothing is found, returns nil
+    def self.find(email)
+        # get the data from the store
+        values = $redis.get db_id(email)
+        # load it
+        if(values.to_s.length > 0)
+          values = JSON.parse(values)
+          return Email.new.setup(values)
+        else
+          nil
+        end
+    end
+
+    # Generates a unique identifier
+    def self.generate_code
+      code = SecureRandom.uuid
+    end
+
+    # Class method
+    # defines the email as this class's id
+    # we use this in the base class for determing
+    # how to store the data related to the class
+    def self.id
+      self.address
+    end
+
+    # Instance method
+    # defines the email as this class's id
+    # we use this in the base class for determing
+    # how to store the data related to the class
+    def id
+      self.address
+    end
+
+    # Loads values from a hash into this object
+    def setup(params)
+      if params.is_a? Hash
+              self.confirmation_code = params["confirmation_code"]
+              self.address           = params["address"]
+              self.confirmed         = params["confirmed"]
+              self.IP                = params["IP"]
+              self.created_at        = params["created_at"]
+              self.sent_at           = params["sent_at"]
+              self.updated_at        = params["updated_at"]
+      else
+        raise Exception, "Invalid parameters class for method. Expecting Hash."
+      end
+      return self
+    end
+
+    # Grab all global global variables in this Object, and turn it into
+    # a hash.
+    def to_hash
+       h =  self.instance_variables.each_with_object({}) { |var,hash|
+       hash[var.to_s.delete("@")] = self.instance_variable_get(var) }
+      # add any values that aren't variables, but are the result of methods:
+      return h
+    end
+
+    # turns this object to a json object
+    def to_json
+      to_hash.to_json
+    end
+
+    # simple accessor for the constant
+    def max_minutes
+      MAX_MINUTES
+    end
+
+    # simple accessor for the constant
+    def max_minutes_in_seconds
+      MAX_MINUTES_IN_SECONDS
+    end
+
+    # enqueues a mail in the gateway, requires the client
+    # ip to be recieved in order to log it.
+    def enqueue
+      # Attempt to queue the email using the preferred
+      # mailing method:
+      if(GMQ.enqueue_confirmation_mail(Email.to_json))
+        self.sent_at = Time.now.utc
+        self.save
+        return
+      else
+        # Our mail gateway is down and we couldn't enqueue
+        raise Exception, "Couldn't enqueue confirmation emails in the mailing system."
+      end
+    end
+
+    def save
+     # update the last updated_at timestamp
+     self.updated_at = Time.now.utc
+     $redis.set db_id, self.to_json
+    end
+
+end
---
+++ app/models/gmq.rb
@@ -0,0 +1,166 @@
+# A class that represents the Government Message Queue.
+# This class allows for quick access to the distributed
+# computing architecture, in order to enqueue jobs.
+class GMQ
+
+  def health_check
+    puts request("/health")
+  end
+
+  def self.enqueue_confirmation_mail(payload)
+    # This method speaks with the CAP API to enqueue an email
+    # If we enqueued successfully:
+      # return true
+    # else
+    # false
+    # puts request("")
+    # test
+    # get('/transaction')
+    post('/mail', payload)
+    true
+  end
+
+  # Perform an HTTP Get against the GMQ
+  def self.get(uri, payload=nil)
+    self.request(uri, method='get', payload)
+  end
+  # Perform an HTTP POST against the GMQ
+  def self.post(uri, payload=nil)
+    self.request(uri, method='post', payload)
+  end
+  # Perform an HTTP DELETE against the GMQ
+  def self.delete(uri, payload=nil)
+    self.request(uri, method='delete', payload)
+  end
+  # Perform an HTTP PUT against the GMQ
+  def self.put(uri, payload=nil)
+    self.request(uri, method='put', payload)
+  end
+
+  # Performs the actual HTTP Requests
+  # against the GMQ
+  def self.request(uri, method='get', payload=nil)
+    site = "#{gmq_url}#{uri}"
+    content_type = nil
+    begin
+      # If have a payload to deliver
+      if(payload)
+        # set the proper content type
+        content_type = :json
+        response = RestClient.send method, site, payload.to_json,
+                                   :content_type => :json,
+                                   :accept => :json
+      # This is a non-payload request
+      else
+        response = RestClient.send method, site
+      end
+      # check response header for errors
+      # if response error inside:
+      # return false
+      # if response no errors inside:
+      puts "HTTP Code:\n#{response.code}\n\n"
+      puts "Headers:\n#{response.headers}\n\n"
+      puts "Result:\n#{response.gsub(",", ",\n").to_str}\n"
+
+      result = JSON.parse(response)
+      return true
+    rescue Errno::ECONNREFUSED => e
+      # connection error - server offline or network failure
+      puts "We got a connection refused, everybody."
+      puts e.inspect.to_s
+    rescue RestClient::Unauthorized => e
+      # 401
+      puts "Bad username or password. Cannot login to the GMQ"
+      puts e.inspect.to_s
+    rescue RestClient::BadRequest => e
+      # 400
+      puts "Bad Request"
+      puts e.inspect.to_s
+    rescue RestClient::Forbidden => e
+      # 403
+      puts "User has no authorization to access that resource."
+    rescue RestClient::ResourceNotFound => e
+      # 404
+      puts "404"
+    rescue RestClient::InternalServerError => e
+      # 500
+      puts "500"
+    rescue RestClient::ServiceUnavailable => e
+      # 503 Service unavailable - maintenance
+      puts "We're in maintenance mode."
+      puts e.inspect.to_s
+
+    rescue Exception => e
+      # If an error ocurred
+      # if(result.has_key? "error")
+      #   puts "THE FOLLOWING ERROR OCURRED: #{result}"
+      # end
+      puts "Class is #{e.class}"
+      puts e.inspect.to_s
+      puts "NOW LETS DO THIS!"
+      error = e.message.strip
+      # if the error isn't empty
+      if(error.to_s.length > 0)
+        # if its likely this is a JSON
+        puts "LEEROY #{error[0]}"
+        # RestClient returns errors in the following form:
+        # 400 Bad Request: {"error":... }
+        # In the documentation it says it does so in this manner:
+        # 400 Bad Request | {"error":... }
+        # In order to be tolerant to future changes, we're
+        # simply going to drop everything that comes before
+        # the actual json.
+        if(error[0] == "{" or error[0] == "[")
+          puts "JENKINS"
+          error = JSON.parse(error)
+
+          # Proceed to check the error type:
+          if(error.has_key? "error")
+            puts error["error"]
+          end
+        end
+      end
+      return false
+    end
+  end
+
+  def self.enqueue_cap_transaction(payload)
+    # This method speaks with the CAP API of the GMQ to generate a transaction
+    # If we enqueued successfully:
+      # true
+    # else
+    # false
+    request("/transaction", 'post', payload)
+  end
+
+  def self.test
+      payload = {
+      		:first_name => "Andrés",
+      		:last_name  => "Colón",
+      		:mother_last_name => "Pérez",
+      		:ssn	=> "123561234",
+      		:license_number => "123456789",
+      		:birth_date => "23/01/19822",
+      		:residency  => "San Juan",
+      		:IP   	    => "192.168.1.1",
+      		:reason	    => "good enough",
+      		:birth_place=> "San Juan",
+      		:email	    => "andres@thoughtware.tv",
+      		:language   => "spanish"
+  	 }
+     self.enqueue_cap_transaction(payload)
+  end
+
+  private
+  def self.basic_authentication
+    "#{ENV["GMQ_CAP_USER"]}:#{ENV["GMQ_CAP_PASSWORD"]}"
+  end
+
+  # Returns the proper URL to access a gmq resource:
+  # http(s)://user:password@host:port
+  def self.gmq_url(https=false)
+    "http#{(https ? "s" : "")}://#{basic_authentication}@"+
+    "#{ENV["GMQ_CAP_HOST"]}:#{ENV["GMQ_CAP_PORT"]}#{ENV["GMQ_CAP_API"]}"
+  end
+
+end
