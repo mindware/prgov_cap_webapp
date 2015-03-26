99373a217911f3781acb0c94465fa82977fe5b17
Andrés Colón <andres@thoughtware.tv>
2015-03-26 04:22:41 -0400

    Updating i18n methods. Tested the safe_html by repeatedly trying to hack it. Tests were satisfactory. Updated the raw method to do a double html_safe, which is only used in very specific circumstances and has a huge warning label on top of it. Updated comments to reflect the actual behaviour of the method.

--- app/helpers/cap_helper.rb
+++ app/helpers/cap_helper.rb
@@ -16,26 +16,26 @@ module PRgovCAPWebApp
 
       # Our raw version of i18n. This method performs internationalization
       # without escaping html. Only use this method if you *absolutely*
-      # need it in order to raw html output. This method as it is unsafe
-      # by default, do not use it unless you know what you're doing.
-      # Do not use this method unless you manually are doing html_safe
-      # to its output. Do not ever use this method without html_safe
-      # if there is user input involved in the text that will be output.
-      #
+      # need it in order to raw html output. This method as it tries to be
+      # unsafe by default, do not use it unless you know what you're doing.
       # This method properly processes accents and special characters
       # found in our locale file into proper HTML entities.
       def i18n_raw(resource, *arr)
         # Rules for special characters conversion to HTML must be handled
         # by I18n transliteration rules.
-        I18n.transliterate(I18n.translate(resource, *arr))
+        i18n_t(resource, *arr).html_safe
       end
 
-      # Our safe to use, html safe version of our i18n_raw.
+      # Our safe to use version of our i18n.
+      # it only unesescapes accents that have would otherwise be
+      # doubly escaped.
       def i18n_t(resource, *arr)
-        i18n_raw(resource, *arr).html_safe
+        I18n.transliterate(I18n.translate(resource, *arr)).html_safe
       end
 
       # Our internationalized asciidoctor
+      # This is not safe, do not allow anything through asciidoc that
+      # has user input. You've been warned - ACP.
       def i18n_asciidoc(resource, *arr)
         # puts "Using ansiidoc on file #{resource}"
         asciidoc(i18n_raw(resource, *arr)).html_safe
