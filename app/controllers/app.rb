5f6fffb813b472642f6b4b0ca1ee62a4779d1652
Andrés Colón <andres@thoughtware.tv>
2015-04-29 14:12:37 -0400

    Adding mobile app controller for mobile app related content.

---
+++ app/controllers/app.rb
@@ -0,0 +1,41 @@
+# A controller to process anything related to the mobile
+# apps.
+# This app component was designed and developed by
+# Andés Colón.
+PRgovCAPWebApp::App.controllers :app do
+
+  include PRgov::Validations
+
+  ############################
+  #         Filters          #
+  ############################
+
+  # Global 'before' check, applies to all resources.
+  # Here we check and set a language cookie. Our cookie is
+  # completely seperate to our 'sessions' (which are only stored
+  # on the server side). These cookies survives
+  # 'session.clear'. We only allow specific pre-approved
+  # language values in the cookie [en, es]
+  before do
+      # If the user has his cookie locale set to english
+      # apply it for this request.
+      if (request.cookies['locale'] == "en")
+          I18n.locale = :en
+      elsif(request.cookies['locale'] == "es")
+          I18n.locale = :es
+      else
+          # if the user is missing the cookie,
+          # give him a spanish cookie.
+          request.cookies['locale'] = "es"
+          # apply the local language
+          I18n.locale = :es
+      end
+  end
+
+  # index provides general info about
+  # the system.
+  get :index, :map => '/app/' do
+    render 'index', :layout => :prgov
+  end
+
+end
